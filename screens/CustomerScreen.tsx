988167f4b70f1e74c38b2b601ead72c3d6e318c8
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-10-30 21:30:08 +0530

    feat: Setup nativewind

---
+++ screens/CustomerScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const CustomerScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>CustomerScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default CustomerScreen;
